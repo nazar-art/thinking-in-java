e5d8b3ac6e4248b5706621e0e3d99f3b2c64966f
Nazar lelyak <nazar.lelyak@gmail.com>
2015-01-11 21:33:13 +0200

    add dynamic impl

---
+++ src/main/java/courses/procedural/dynamic/DoubleNode.java
@@ -0,0 +1,13 @@
+package courses.procedural.dynamic;
+
+public class DoubleNode {
+    public int value;
+    public DoubleNode prev;
+    public DoubleNode next;
+
+    public DoubleNode(int value, DoubleNode prev, DoubleNode next) {
+        this.value = value;
+        this.prev = prev;
+        this.next = next;
+    }
+}
