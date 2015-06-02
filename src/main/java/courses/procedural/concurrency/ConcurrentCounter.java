fbb07ec4202900c8b0047530f1e00a90812b202f
Nazar Lelyak <nazar_lelyak@epam.com>
2015-06-02 16:33:07 +0300

    add some conc code

---
+++ src/main/java/courses/procedural/concurrency/ConcurrentCounter.java
@@ -0,0 +1,26 @@
+package courses.procedural.concurrency;
+
+public class ConcurrentCounter {
+    public static final int N = 1000_000_000;
+    public static int counter = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t0 = new Thread(new Runnable() {
+            public void run() {
+                for (int k = 0; k < N; k++) counter++;
+            }
+        });
+        t0.start();
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                for (int k = 0; k < N; k++) counter++;
+            }
+        });
+        t1.start();
+
+        t0.join();
+        t1.join();
+
+        System.out.println(counter);
+    }
+}
